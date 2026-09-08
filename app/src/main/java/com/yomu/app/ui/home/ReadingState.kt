62dcc2a6f44d4d5c4d1919b07678165b22091a4b
Arthur Saraiva <arthur.m.saraiva@hotmail.com>
2026-09-08 14:50:14 +0100

    feat: define reading setup readiness

---
+++ app/src/main/java/com/yomu/app/ui/home/ReadingState.kt
@@ -0,0 +1,17 @@
+package com.yomu.app.ui.home
+
+import com.yomu.app.db.entities.ModelStatus
+import com.yomu.core.Constants
+
+enum class Readiness { Ready, NeedsModels, NeedsPermission, NeedsBoth }
+
+fun resolveReadiness(models: Map<String, ModelStatus>, hasOverlayPermission: Boolean): Readiness {
+    val modelsReady = models[Constants.BUBBLE_DETECTION_MODEL_ID] == ModelStatus.READY &&
+        models[Constants.MANGA_OCR_MODEL_ID] == ModelStatus.READY
+    return when {
+        modelsReady && hasOverlayPermission -> Readiness.Ready
+        modelsReady -> Readiness.NeedsPermission
+        hasOverlayPermission -> Readiness.NeedsModels
+        else -> Readiness.NeedsBoth
+    }
+}
